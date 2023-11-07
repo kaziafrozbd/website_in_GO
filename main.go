9cd9e4ec3a8c6b2c48e7e31a6b26a94f0ef9f8b4
Kazi_Afroz <k.afroz.bd@gmail.com>
2023-11-07 22:07:49 +0600

    database added

--- main.go
+++ main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"text/template"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+var db *sql.DB
+var err error
+func init(){
+	
+    // Open up our database connection.
+    // I've set up a database on my local machine using phpmyadmin.
+    // The database is called testDb
+    db, err = sql.Open("mysql", "root:hosting123@tcp(127.0.0.1:3306)/webhosting_db")
+
+    // if there is an error opening the connection, handle it
+    if err != nil {
+        panic(err.Error())
+    }
+
+    // defer the close till after the main function has finished
+    // executing
+    // defer db.Close()
+
+	
+    
+	fmt.Println("db connected..")
+}
 
 func main() {
 
@@ -54,15 +79,29 @@ func docs(w http.ResponseWriter, r *http.Request){
 }
 func request(w http.ResponseWriter, r *http.Request){
 	
-	// name := r.FormValue("name")
-	// comapny := r.FormValue("company")
-	// email := r.FormValue("email")
+	name := r.FormValue("name")
+	company := r.FormValue("company")
+	email := r.FormValue("email")
 
 	// fmt.Println(name, comapny, email)
 	// fmt.Fprintf(w,"received \n %s \n %s \n %s", name, comapny, email)
-	r.ParseForm()
-	for key, val := range r.Form{
-		fmt.Println(key, val)
-	}
+	// r.ParseForm()
+	// for key, val := range r.Form{
+	// 	fmt.Println(key, val)
+	// }
+
+	// perform a db.Query insert
+	sq := "INSERT INTO `request` (`id`, `name`, `company`, `email`, `status`) VALUES (NULL, '%s', '%s', '%s', '1')"
+	sql := fmt.Sprintf(sq, name, company, email)
+
+	// fmt.Println(sql)
+    insert, err := db.Query(sql)
+
+    // if there is an error inserting, handle it
+    if err != nil {
+        panic(err.Error())
+    }
+    // be careful deferring Queries if you are using transactions
+    defer insert.Close()
 	fmt.Fprintf(w,`ok`)
 }
